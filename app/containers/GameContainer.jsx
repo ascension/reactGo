9e6ebe8d69e16c501bdb64f694df4561b70a6bad
Jerrod Bunce <jerrod@engagereal.com>
2016-11-15 10:09:34 -0700

    Get Coinflip working!

--- app/containers/GameContainer.jsx
+++ app/containers/GameContainer.jsx
@@ -7,24 +7,78 @@ import { Circle } from 'rc-progress';
 
 import GamePlayer from '../components/GamePlayer';
 
+let tm;
+
 class GameContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      percent: '100'
-    }
+      percent: 100,
+      timeRemaining: 5.0,
+      isFlipping: false,
+      gameEnded: false,
+    };
+
+    this.getStrokeColor = this.getStrokeColor.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.startCountDown = this.startCountDown.bind(this);
   }
 
   componentDidMount() {
     // TODO - Fetch Game by id.
     // this.props.params.gameId
-    setInterval(() => {
-      console.log('blah');
-      this.setState({
-        percent: this.state.percent - 10
-      });
-    }, 1000);
+
+  }
+
+  getStrokeColor() {
+    if (this.state.percent > 75) {
+      return '#19FF65';
+    } else if(this.state.percent > 50) {
+      return '#FF9900';
+    } else {
+      return '#db4437';
+    }
+  }
+
+  startCountDown() {
+    tm = setInterval(() => {
+      if (this.state.timeRemaining <= 0) {
+        clearInterval(tm);
+        this.setState({
+          isFlipping: true,
+          percent: 0,
+          timeRemaining: 0,
+        }, () => setTimeout(() => {
+          this.setState({
+            isFlipping: false,
+            gameEnded: true
+          })
+        }, 5000));
+      } else {
+
+        const tick = this.state.timeRemaining - 0.2;
+
+        this.setState({
+          isFlipping: false,
+          percent: tick > 0 ? tick * 20 : 0,
+          timeRemaining: tick
+        });
+      }
+    }, 200);
+  }
+
+  startGame() {
+    this.setState({
+      percent: 100,
+      timeRemaining: 5.0,
+      isFlipping: false
+    }, this.startCountDown());
+  }
+
+  handleClick() {
+    this.startGame()
   }
 
   render() {
@@ -55,10 +109,18 @@ class GameContainer extends Component {
         <div className={cx('players')}>
           <GamePlayer/>
           <span className={cx('ticker')}>
-            <Circle style={{width: '100px', margin: '0 auto'}} percent={this.state.percent} strokeWidth="4" strokeColor="#D3D3D3" />
+            <div className={cx('flip')}>
+                <div className={cx('card', {flipped: this.state.isFlipping, gameEnded: this.state.gameEnded})}>
+                    <div className={cx('face', 'front')}>Heads</div>
+                    <div className={cx('face', 'back')}>Tails</div>
+                </div>
+            </div>
+            <Circle style={{width: '205px', margin: '0 auto', position: 'absolute', top: '50px', left: '480px'}} percent={this.state.percent} strokeWidth="4" strokeColor={this.getStrokeColor()} />
             Flipping in....
             <br/>
-            7.8sec
+            {this.state.timeRemaining.toFixed(2)} sec
+            <br/>
+            <button onClick={this.handleClick} disabled={this.state.disable}>Flip</button>
           </span>
           <GamePlayer/>
         </div>
