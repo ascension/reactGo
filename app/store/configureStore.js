b571ac30638386aad1b012d49833284aadc5f9a2
Jerrod Bunce <jerrod@engagereal.com>
2016-12-20 21:33:35 -0700

    no message

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -8,7 +8,7 @@ import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 import { browserHistory } from 'react-router';
 
-let socket = io('https://cryptoduel.com');
+let socket = io('https://cryptoduel.com:443');
 
 socket.on('#navigate', function(path){
   browserHistory.push(path);
