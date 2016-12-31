b16efb9732e4285dfa320227ffbba5d4d3f962d8
Jerrod Bunce <jerrod@engagereal.com>
2016-12-31 14:56:39 -0700

    Fix

--- server/db/index.js
+++ server/db/index.js
@@ -18,33 +18,6 @@ switch (DB_TYPE) {
     throw new Error(`No database type '${DB_TYPE}' found`);
 }
 
-//Require bitcore
-var bitcore = require('bitcore-node');
-
-//Services
-var Bitcoin = bitcore.services.Bitcoin;
-var Web = bitcore.services.Web;
-
-var myNode = new bitcore.Node({
-  network: 'regtest',
-  services: [
-    {
-      name: 'bitcoind',
-      module: BitcoinService,
-      config: {
-        connect: [
-          {
-            rpchost: '127.0.0.1',
-            rpcport: 30521,
-            rpcuser: 'bitcoin',
-            rpcpassword: 'local321',
-            zmqpubrawtx: 'tcp://127.0.0.1:30611'
-          },
-        ]
-      }
-    }
-  ]
-});
 export const connect = dbConfig.connect;
 export const controllers = dbConfig.controllers;
 export const passport = dbConfig.passport;
